fbafea376a12ae2163c9144791d1ce311b7ea696
harrybark <harrypmw@gmail.com>
2024-03-03 19:11:36 +0900

    #5-조회 기능(단건)
    CounselId를 인자로 받으면, 단건을 조회하고
    조회되는 케이스가 없으면 에러를 리턴하도록 서비스, 컨트롤러 구현

--- src/main/java/com/devtomato/loan/service/CounselService.java
+++ src/main/java/com/devtomato/loan/service/CounselService.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CounselService
 {
-
     Response create(Request request);
+
+    Response get(Long counselId);
 }
--- src/main/java/com/devtomato/loan/service/CounselServiceImpl.java
+++ src/main/java/com/devtomato/loan/service/CounselServiceImpl.java
@@ -3,6 +3,8 @@ package com.devtomato.loan.service;
 import com.devtomato.loan.domain.Counsel;
 import com.devtomato.loan.dto.CounselDTO.Request;
 import com.devtomato.loan.dto.CounselDTO.Response;
+import com.devtomato.loan.exception.BaseException;
+import com.devtomato.loan.exception.ResultType;
 import com.devtomato.loan.repository.CounselRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -26,5 +28,13 @@ public class CounselServiceImpl implements CounselService
         Counsel saved = counselRepository.save(counsel);
 
         return modelMapper.map(saved, Response.class);
+    }
+
+    @Override
+    public Response get(Long counselId) {
+        Counsel counsel = counselRepository.findById(counselId).orElseThrow(() -> {
+            throw new BaseException(ResultType.SYSTEM_ERROR);
+        });
+        return modelMapper.map(counsel, Response.class);
     }
 }
